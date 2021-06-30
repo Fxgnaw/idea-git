b9526e740b6169ed369db650960b314edf8c0e2e
Fxgnaw <807275312@qq.com>
2021-06-30 14:18:16 +0800

    Update newnew.java

--- src/main/java/test/newnew.java
+++ src/main/java/test/newnew.java
@@ -5,7 +5,9 @@ public class newnew {
     static int a = 20;
 
     private static int test() {
-        a = a + 20;
+        for(int i=0;i<10;i++){
+            a=a+i;
+        }
         return a;
     }
 
